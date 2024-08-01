556fa521f4f650917301dba1df71796f14f514a2
maksym-alavatskyi <maksym.alavatskyi@contentsquare.com>
2024-08-01 15:23:21 +0200

    TRANSLATE-1: translate to UA

--- src/modules/account/components/login/index.tsx
+++ src/modules/account/components/login/index.tsx
@@ -14,15 +14,18 @@ const Login = ({ setCurrentView }: Props) => {
   const [message, formAction] = useFormState(logCustomerIn, null)
 
   return (
-    <div className="max-w-sm w-full flex flex-col items-center" data-testid="login-page">
-      <h1 className="text-large-semi uppercase mb-6">Welcome back</h1>
+    <div
+      className="max-w-sm w-full flex flex-col items-center"
+      data-testid="login-page"
+    >
+      <h1 className="text-large-semi uppercase mb-6">Раді бачити знов</h1>
       <p className="text-center text-base-regular text-ui-fg-base mb-8">
-        Sign in to access an enhanced shopping experience.
+        Увійдіть, щоб отримати додатковий доступ до покупок.
       </p>
       <form className="w-full" action={formAction}>
         <div className="flex flex-col w-full gap-y-2">
           <Input
-            label="Email"
+            label="Пошта"
             name="email"
             type="email"
             title="Enter a valid email address."
@@ -31,7 +34,7 @@ const Login = ({ setCurrentView }: Props) => {
             data-testid="email-input"
           />
           <Input
-            label="Password"
+            label="Пароль"
             name="password"
             type="password"
             autoComplete="current-password"
@@ -40,16 +43,18 @@ const Login = ({ setCurrentView }: Props) => {
           />
         </div>
         <ErrorMessage error={message} data-testid="login-error-message" />
-        <SubmitButton data-testid="sign-in-button" className="w-full mt-6">Sign in</SubmitButton>
+        <SubmitButton data-testid="sign-in-button" className="w-full mt-6">
+          Увійдіть
+        </SubmitButton>
       </form>
       <span className="text-center text-ui-fg-base text-small-regular mt-6">
-        Not a member?{" "}
+        Не маєте власний кабінет?{" "}
         <button
           onClick={() => setCurrentView(LOGIN_VIEW.REGISTER)}
           className="underline"
           data-testid="register-button"
         >
-          Join us
+          Доєднуйтесь
         </button>
         .
       </span>
